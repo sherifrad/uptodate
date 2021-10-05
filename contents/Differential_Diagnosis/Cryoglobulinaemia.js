28611097fb7222ec7a337e568aaf5ab44066f61f
Sherif <dr.sherif1992@gmail.com>
2021-10-06 00:57:15 +0200

    Add files via upload

---
+++ contents/Differential_Diagnosis/Cryoglobulinaemia.js
@@ -0,0 +1,12 @@
+content=["<dl>",
+"<dt><a href='#Section0'>Small to medium vessel vasculitis</a></dt>",
+"<dd id='Section0'>",
+"<p><strong>Sign & Symptoms:</strong> No differentiating signs/symptoms.",
+"<br><strong>Test:</strong> Vasculitis mimickers such as subacute endocarditis need to be identified urgently. Vasculitis can be life threatening, especially if there is significant multiple organ failure.Because medium-vessel vasculitis may occur due to underlying viral infections, symptoms of mesenteric ischaemia, evidence of cardiac decompensation, or hypertension should also be sought.Blood cultures, echocardiogram, HIV test, antiphospholipid antibodies, lupus anticoagulant, and other infectious-disease workup suggested by the hx (e.g., Rocky mountain spotted serologies) should be performed.A FBC, comprehensive blood chemistry profile, and urinalysis should be carried out to measure the extent of vasculitis. [4]Antineutrophil antibodies (ANCA), ANA, extractable nuclear antigen antibody (ENA), dsDNA, complement assay, eosinophil count, and specific biopsies (e.g., IgA deposits on skin or kidney suggesting Henoch-Schonlein purpura) should be ordered to establish aetiology.</p>",
+"</dd>",
+"<dt><a href='#Section1'>Cryofibrinogenaemia</a></dt>",
+"<dd id='Section1'>",
+"<p><strong>Sign & Symptoms:</strong> Acute unexplained areas of tissue ischaemia and gangrene; rare disorder. [32]",
+"<br><strong>Test:</strong> Cryofibrinogenaemia is a rare disorder that should be considered in any patient presenting with acute unexplained areas of tissue ischaemia and gangrene. [32]  Cryoglobulins and other causes of vaso-occlusive diseases should be excluded.Cryofibrinogens are detected by cold precipitation.</p>",
+"</dd>",
+"</dl>"].join("\n");
