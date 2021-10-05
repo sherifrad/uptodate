ff76883322e1e39a82a55656575edd122eb8e97d
Sherif <dr.sherif1992@gmail.com>
2021-10-06 00:57:21 +0200

    Add files via upload

---
+++ contents/Differential_Diagnosis/Gestational_diabetes.js
@@ -0,0 +1,12 @@
+content=["<dl>",
+"<dt><a href='#Section0'>type 1 diabetes</a></dt>",
+"<dd id='Section0'>",
+"<p><strong>Sign & Symptoms:</strong> There may be no difference in signs and symptoms, although glucose level deterioration and tendency for ketosis is generally greater. [1]  Women with type 1 diabetes are more likely to be white and lean.Clinical evidence of diabetic complications, such as neuropathy, retinopathy, and albuminuria, would indicate longer-standing hyperglycaemia that antedated pregnancy.[C Evidence]",
+"<br><strong>Test:</strong> Anti-beta cell serologies (e.g., glutamic acid decarboxylase-65 [GAD-65] and anti-insulin antibodies) and postpartum glycaemia testing can be performed. [1]The finding of a markedly elevated HbA1c level or fasting plasma glucose, especially early in pregnancy, suggests that diabetes existed before pregnancy.</p>",
+"</dd>",
+"<dt><a href='#Section1'>type 2 diabetes</a></dt>",
+"<dd id='Section1'>",
+"<p><strong>Sign & Symptoms:</strong> There may be no difference in signs and symptoms in women with GDM who had unrecognised type 2 diabetes before pregnancy.Compared with women with autoimmune (type 1) diabetes, women with type 2 diabetes are more likely to be obese, have a family history of type 2 diabetes, and exhibit evidence of insulin resistance, such as dyslipidaemia, acanthosis nigricans, or history suggesting polycystic ovarian syndrome.Clinical evidence of diabetic complications, such as neuropathy, retinopathy, and albuminuria, would indicate longer-standing hyperglycaemia that antedated pregnancy.[C Evidence]",
+"<br><strong>Test:</strong> The finding of a markedly elevated HbA1c level or fasting plasma glucose, especially early in pregnancy, suggests that diabetes existed before pregnancy. [1]</p>",
+"</dd>",
+"</dl>"].join("\n");
